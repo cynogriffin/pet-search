daf543365b9d34b7b10078693aa4ac3dd2c11ea6
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-18 15:37:54 -0600

    added picture of the day.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 
 let apiURL1 = 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=10&offset=10'
 let apiURL2 = 'https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?hide_recent_previous=false&include_suborbital=true&is_crewed=false&limit=10&offset=10&related=false'
+let apiPictureoftheday = 'https://api.nasa.gov/planetary/apod?api_key=X9BF4XMGzgnLciHZqS1xFfjszIgdKrMIUe6DWnoC'
+console.log(apiPictureoftheday)
 
 // second api work for spaceflight news articles
 // url for api call for news
@@ -51,59 +53,53 @@ getNews().catch(error => {
     console.error(error);
 });
 
+fetch(apiURL2)
+    .then(function (response) {
+        if (response.ok) {
+
+            response.json().then(function (data) {
+
+                for (let i = 1; i < 10; i++) {
+                    let spacecraft = data.results[i].name;
+                    let launchDate = data.results[i].window_start
+                    let launchDatesFormated = moment(data.results[i].window_start).format('dddd, MMMM Do YYYY, h:mm:ss a');
+                    const div = document.createElement('div');
 
 
-// fetch(apiURL1)
+                    let launchDateDisplay = `<h2> Spacecraft: ${spacecraft} Launch Date: ${launchDatesFormated}</h2>`
+                    const container = document.getElementById('launch-Date');
 
-// .then(function (response) {
-//     if (response.ok) {
-//        response.json().then(function (data) {
+                    container.append(div);
+
+                    div.innerHTML = launchDateDisplay
+                }
+            })
+        }
+    })
+
+
+    
+fetch(apiPictureoftheday)
+    .then(function (response) {
+        if (response.ok) {
+
+            response.json().then(function (data) {
+                let pic = data.url;
+                let explanation = data.explanation;
+                let copyright = data.copyright;
+                const div = document.createElement('div');
+               
+
+                let picoftheday = `<img src=${pic}>`
 
         
-      
-//                    for (let i = 1; i < 10; i++) {
-//                        let spacecraft = data.results[i].name;
-//                        let launchDate = data.results[i].window_start
-//                        const div = document.createElement('div');
 
-//                        let spacecraftDisplay = `<h2> Spacecraft: ${spacecraft}</h2>`
-//                         let launchDateDisplay = `<h2> Spacecraft: ${launchDate}</h2>`
-                      
-//                        const container = document.getElementById('spacecraftName');
-//                        container.append(div);
-
-//                        div.innerHTML = spacecraftDisplay + launchDateDisplay
-
-//                    }
-    
-
-// })
-// }})
-
-fetch(apiURL2)
-.then(function (response) {
-    if (response.ok) {
-
-       response.json().then(function (data) {
-    
-        for (let i = 1; i < 10; i++) {
-            let spacecraft = data.results[i].name;
-            let launchDate = data.results[i].window_start
-            let launchDatesFormated = moment(data.results[i].window_start).format('dddd, MMMM Do YYYY, h:mm:ss a');
-            const div = document.createElement('div');
-
-           
-             let launchDateDisplay = `<h2> Spacecraft: ${spacecraft} Launch Date: ${launchDatesFormated}</h2>`
-            const container = document.getElementById('launch-Date');
-            
-            container.append(div);
-
-            div.innerHTML = launchDateDisplay 
-        }})}})
+                const container = document.getElementById('picture-of-the-day');
+              container.append(div);
 
 
-    
-
-
- 
-  
+              div.innerHTML = picoftheday + explanation + copyright 
+                
+            })
+        }
+    })
