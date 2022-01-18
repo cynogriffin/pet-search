a7071235538efeb90df5b4deac07dfd5863c57db
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-01-17 20:11:02 -0600

    first Api - spacecraft name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
-let apiURL1 = 'll.thespacedevs.com/2.2.0/location/?pad_location_id'
+
+let apiURL1 = 'https://lldev.thespacedevs.com/2.2.0/spacecraft/?limit=10&offset=10'
+
+fetch(apiURL1)
+//   .then(response => response.json())
+//   .then(data => console.log(data.results[1].name));
+.then(function (response) {
+    if (response.ok) {
+
+       response.json().then(function (data) {
 
 
+ for (let i = 1; i < 10; i++) {
+    let spacecraft = data.results[i].name;
+    
+    const div = document.createElement('div');
+let spacecraftDisplay = `<h2> Spacecraft: ${spacecraft}</h2>`
+    
+div.innerHTML = spacecraftDisplay
 
-   fetch(apiURL1).then(function (response) {
-    console.log(response)
-   })
+
+    const container = document.getElementById('spacecraftName');
+    container.append(div);
+
+}}
+)}}
+)
+
+  
+  
+
+
+ 
+  
